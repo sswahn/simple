5916c58834305db4f71ee28a2d86d2729b929d9f
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-12 13:38:23 -0500

    Update create.js

--- src/core/create.js
+++ src/core/create.js
@@ -2,7 +2,7 @@ import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
 
 const create = async (data, key) => {
   if (!process.env.BUCKET_NAME) {
-    throw new ReferenceError('S3_BUCKET_NAME environmental variable is required.')
+    throw new ReferenceError('BUCKET_NAME environmental variable is required.')
   }
   if (typeof process.env.BUCKET_NAME !== 'string') {
     throw new TypeError('BUCKET_NAME must be of type string.')
