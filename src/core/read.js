b7e844d675b173e1202319e6cab82d9924a40e29
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-12 14:07:52 -0500

    Update read.js

--- src/core/read.js
+++ src/core/read.js
@@ -1,18 +1,23 @@
 import { S3Client, ListObjectsCommand } from '@aws-sdk/client-s3'
 
-const read = async () => {
+const read = async (limit = undefined) => {
   if (!process.env.BUCKET_NAME) {
     throw new ReferenceError('BUCKET_NAME environmental variable is required.')
   }
   if (typeof process.env.BUCKET_NAME !== 'string') {
     throw new TypeError('BUCKET_NAME must be of type string.')
   }
+  if (limit && typeof limit !== 'number') {
+    throw new TypeError('Argument must be of type number.')
+  }
   try {
-    const client = new S3Client()
-    const params = {
-      Bucket: process.env.S3_BUCKET_NAME
+    const limitation = limit ? { Limit: limit } : {}
+    const values = {
+      Bucket: process.env.S3_BUCKET_NAME,
+      ...limitation
     }
-    const command = new ListObjectsCommand(params)
+    const client = new S3Client()
+    const command = new ListObjectsCommand(values)
     return client.send(command)
   } catch (error) {
     throw new Error(`read: ${error}`)
