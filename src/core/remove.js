427f47f3292ca775861b3bdfc7cc35cab1591b93
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-12 13:44:15 -0500

    Update remove.js

--- src/core/remove.js
+++ src/core/remove.js
@@ -7,15 +7,15 @@ const remove = async key => {
   if (typeof process.env.BUCKET_NAME !== 'string') {
     throw new TypeError('BUCKET_NAME must be of type string.')
   }
-  if (!key) {
-    throw new TypeError('Key is required for deleteObject.')
+  if (typeof key !== 'object') {
+    throw new TypeError('Argument must be of type object.')
   }
   try {
     const client = new S3Client()
     const params = {
       Bucket: process.env.BUCKET_NAME,
       Key: key
-    };
+    }
     const command = new DeleteObjectCommand(params)
     return client.send(command)
   } catch (error) {
