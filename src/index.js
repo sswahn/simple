3c17f901cc190b5d8918dd7f2dbad210e8f4af50
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-12 13:51:20 -0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import create from './core/create.js'
+import read from './core/read.js'
+import readOne from './core/readOne.js'
+import update from './core/update.js'
+import remove from './core/remove.js'
+
+const simple = {
+  create,
+  read,
+  readOne,
+  update,
+  remove
+}
+
+export default simple
